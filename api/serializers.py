6a6295203708fbd13696011e5a761e8a17c364b0
Joshua <okorie@glade.ng>
2024-03-23 13:00:26 +0100

    removed password from list users

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     username = serializers.CharField(required=True)
     first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
@@ -29,4 +29,4 @@ class UserSerializer(serializers.ModelSerializer):
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'date_joined', 'password')
+        fields = ('id', 'username', 'email', 'date_joined')
